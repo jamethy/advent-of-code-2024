99f219edaa53ae60788a955dcbadd8029d13aca7
James Burns <jamesianburns@gmail.com>
2024-12-15 15:07:39 -0500

    day 15 stuck

--- advent15/advent15.go
+++ advent15/advent15.go
@@ -3,10 +3,17 @@ package advent15
 import (
 	"advent2024/util"
 	"bytes"
+	"fmt"
 	"strings"
 )
 
 func Solution(inputFile string) (part1, part2 any) {
+	part1 = part1Solution(inputFile)
+	part2 = part2Solution(inputFile)
+	return part1, part2
+}
+
+func part1Solution(inputFile string) int {
 	parts := util.ReadFileSplitBy(inputFile, "\n\n")
 
 	grid := bytes.Split([]byte(parts[0]), []byte("\n"))
@@ -24,7 +31,7 @@ robotSearch:
 	}
 
 	for _, dir := range directions {
-		rI, rJ = move(grid, rI, rJ, dir)
+		rI, rJ = part1Move(grid, rI, rJ, dir)
 	}
 
 	part1Sum := 0
@@ -41,24 +48,13 @@ robotSearch:
 	//	fmt.Println(string(line))
 	//}
 
-	return part1Sum, 0
+	return part1Sum
 }
 
-func move(grid [][]byte, i, j int, dir rune) (int, int) {
+func part1Move(grid [][]byte, i, j int, dir rune) (int, int) {
 
-	ni, nj := 0, 0
-	switch dir {
-	case '<':
-		ni, nj = i, j-1
-	case '>':
-		ni, nj = i, j+1
-	case '^':
-		ni, nj = i-1, j
-	case 'v':
-		ni, nj = i+1, j
-	default:
-		panic("unrecognized direction")
-	}
+	di, dj := dirFromRune(dir)
+	ni, nj := i+di, j+dj
 
 	switch grid[ni][nj] {
 	case '#':
@@ -68,7 +64,7 @@ func move(grid [][]byte, i, j int, dir rune) (int, int) {
 		grid[i][j] = '.'
 		return ni, nj
 	case 'O':
-		move(grid, ni, nj, dir)
+		part1Move(grid, ni, nj, dir)
 		if grid[ni][nj] == '.' {
 			grid[ni][nj] = grid[i][j]
 			grid[i][j] = '.'
@@ -77,5 +73,163 @@ func move(grid [][]byte, i, j int, dir rune) (int, int) {
 		return i, j
 	default:
 		panic("unrecognized character")
+	}
+}
+
+func part2Solution(inputFile string) int {
+	parts := util.ReadFileSplitBy(inputFile, "\n\n")
+
+	originalGrid := bytes.Split([]byte(parts[0]), []byte("\n"))
+	directions := strings.ReplaceAll(parts[1], "\n", "")
+	_ = directions
+
+	grid := make([][]byte, len(originalGrid))
+
+	rI, rJ := 0, 0
+	_, _ = rI, rJ
+	for i, line := range originalGrid {
+		grid[i] = make([]byte, 2*len(originalGrid[0]))
+		for j, c := range line {
+			nj := 2 * j
+
+			switch c {
+			case '@':
+				rI, rJ = i, nj
+				grid[i][nj] = '@'
+				grid[i][nj+1] = '.'
+			case '.':
+				grid[i][nj] = '.'
+				grid[i][nj+1] = '.'
+			case 'O':
+				grid[i][nj] = '['
+				grid[i][nj+1] = ']'
+			case '#':
+				grid[i][nj] = '#'
+				grid[i][nj+1] = '#'
+			}
+		}
+	}
+	//for _, line := range grid {
+	//	fmt.Println(string(line))
+	//}
+	//
+	for _, dir := range directions {
+		fn := makePart2MoveFunc(grid, rI, rJ, dir)
+		if fn != nil {
+			fn()
+			di, dj := dirFromRune(dir)
+			rI, rJ = rI+di, rJ+dj
+		}
+
+		//fmt.Println("\nMove " + string(dir) + ":")
+		//for _, line := range grid {
+		//	fmt.Println(string(line))
+		//}
+		//time.Sleep(10 * time.Second)
+	}
+
+	for _, line := range grid {
+		fmt.Println(string(line))
+	}
+
+	part2Sum := 0
+	for i, line := range grid {
+		for j, c := range line {
+			if c != '[' {
+				continue
+			}
+			part2Sum += 100*i + j
+		}
+	}
+
+	return part2Sum
+}
+
+func dirFromRune(r rune) (int, int) {
+	switch r {
+	case '<':
+		return 0, -1
+	case '>':
+		return 0, 1
+	case '^':
+		return -1, 0
+	case 'v':
+		return 1, 0
+	default:
+		panic("unrecognized direction")
+	}
+}
+
+func makePart2MoveFunc(grid [][]byte, i, j int, dir rune) func() {
+	if grid[i][j] == '#' {
+		return nil
+	}
+	if grid[i][j] == '.' {
+		return func() {}
+	}
+
+	di, dj := dirFromRune(dir)
+
+	if grid[i][j] == '@' {
+		ni, nj := i+di, j+dj
+		prev := makePart2MoveFunc(grid, ni, nj, dir)
+		if prev == nil {
+			return nil
+		}
+		return func() {
+			prev()
+			grid[ni][nj] = '@'
+			grid[i][j] = '.'
+		}
+	}
+	if grid[i][j] == ']' {
+		j--
+	}
+
+	switch dir {
+	case '<':
+		ni, nj := i+di, j+dj
+		prev := makePart2MoveFunc(grid, ni, nj, dir)
+		if prev == nil {
+			return nil
+		}
+		return func() {
+			prev()
+			grid[i][nj] = '['
+			grid[i][j] = ']'
+			grid[i][j+1] = '.'
+		}
+	case '>':
+		ni, nj := i+di, j+dj
+		prev := makePart2MoveFunc(grid, ni, nj+1, dir)
+		if prev == nil {
+			return nil
+		}
+		return func() {
+			prev()
+			grid[i][j] = '.'
+			grid[i][nj] = '['
+			grid[i][nj+1] = ']'
+		}
+	case '^', 'v':
+		ni := i + di
+		leftPrev := makePart2MoveFunc(grid, ni, j, dir)
+		if leftPrev == nil {
+			return nil
+		}
+		rightPrev := makePart2MoveFunc(grid, ni, j+1, dir)
+		if rightPrev == nil {
+			return nil
+		}
+		return func() {
+			leftPrev()
+			rightPrev()
+			grid[ni][j] = '['
+			grid[ni][j+1] = ']'
+			grid[i][j] = '.'
+			grid[i][j+1] = '.'
+		}
+	default:
+		panic("unexpected direction")
 	}
 }
--- advent15/advent15_test.go
+++ advent15/advent15_test.go
@@ -11,29 +11,64 @@ func TestSolution(t *testing.T) {
 		wantPart1 any
 		wantPart2 any
 	}{
-		{
-			name:      "small",
-			wantPart1: 2028,
-			wantPart2: 0,
-		},
+		//{
+		//	name:      "small",
+		//	wantPart1: 2028,
+		//	wantPart2: 0,
+		//},
+		//{
+		//	name:      "small-2",
+		//	wantPart1: 0,
+		//	wantPart2: 0,
+		//},
 		{
 			name:      "sample",
 			wantPart1: 10092,
-			wantPart2: 0,
+			wantPart2: 9021,
 		},
 		{
 			name:      "input",
 			wantPart1: 1514353,
-			wantPart2: 0,
+			wantPart2: 0, // 1,515,986 too low
+		},
+		{
+			name:      "reddit-1",
+			wantPart1: 0,
+			wantPart2: 1430,
+		},
+		{
+			name:      "reddit-2",
+			wantPart1: 0,
+			wantPart2: 406,
+		},
+		{
+			name:      "reddit-3",
+			wantPart1: 0,
+			wantPart2: 509,
+		},
+		{
+			name:      "reddit-4",
+			wantPart1: 0,
+			wantPart2: 822,
+		},
+		{
+			name:      "reddit-5",
+			wantPart1: 0,
+			wantPart2: 511,
+		},
+		{
+			name:      "reddit-6",
+			wantPart1: 0,
+			wantPart2: 816,
 		},
 	}
 	for _, tt := range tests {
-		gotPart1, gotPart2 := Solution(tt.name + ".txt")
-		t.Run(tt.name+"-part1", func(t *testing.T) {
-			if !reflect.DeepEqual(gotPart1, tt.wantPart1) {
-				t.Errorf("Solution() gotPart1 = %v, want %v", gotPart1, tt.wantPart1)
-			}
-		})
+		_, gotPart2 := Solution(tt.name + ".txt")
+		//t.Run(tt.name+"-part1", func(t *testing.T) {
+		//	if !reflect.DeepEqual(gotPart1, tt.wantPart1) {
+		//		t.Errorf("Solution() gotPart1 = %v, want %v", gotPart1, tt.wantPart1)
+		//	}
+		//})
 		t.Run(tt.name+"-part2", func(t *testing.T) {
 			if !reflect.DeepEqual(gotPart2, tt.wantPart2) {
 				t.Errorf("Solution() gotPart2 = %v, want %v", gotPart2, tt.wantPart2)
